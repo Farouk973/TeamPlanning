520dab4b145788e84541feb8c11755c321140db9
Raedjd <raedjaidi.work@gmail.com>
2023-02-22 10:57:17 +0100

    add top-nav component

--- src/app/app-entry-point/app-entry-point.module.ts
+++ src/app/app-entry-point/app-entry-point.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
 import { SideBarComponent } from './layout/side-bar/side-bar.component';
+import { TopNavComponent } from './layout/top-nav/top-nav.component';
 
 
 
 @NgModule({
   declarations: [
     NavBarComponent,
-    SideBarComponent
+    SideBarComponent,
+    TopNavComponent
   ],
   imports: [
     CommonModule
