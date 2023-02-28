dd22cdbc40e6f732e566c463ec9361eddca29d7e
sabri krima <Skrima@naxxum.fr>
2023-02-28 13:57:35 +0100

    add(teamPlanningFront):Add Side-bar et Top-Bar

--- src/app/app-entry-point/layout/top-nav/top-nav.component.ts
+++ src/app/app-entry-point/layout/top-nav/top-nav.component.ts
@@ -1,17 +1,28 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-top-nav',
   templateUrl: './top-nav.component.html',
-  styleUrls: ['./top-nav.component.css']
+  styleUrls: ['./top-nav.component.scss']
 })
 export class TopNavComponent implements OnInit {
+  @Output() sideNavToggled = new EventEmitter<void>();
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    ) {
+ 
   }
 
+  ngOnInit() { }
+
+  toggleSidebar() {
+    this.sideNavToggled.emit();
+  }
+  onLoggedout() {
+  }
 
 }
