88304afc2037478ed085dd325a731d248c0bcf18
Raedjd <raedjaidi.work@gmail.com>
2023-03-04 20:05:55 +0100

    Add avatar in profil menu

--- src/app/app-entry-point/dashboard/profile/profile.component.ts
+++ src/app/app-entry-point/dashboard/profile/profile.component.ts
@@ -10,10 +10,18 @@ import { OidcSecurityService } from 'angular-auth-oidc-client';
 export class ProfileComponent implements OnInit {
   profileJson: string | null = null;
 
+  fileName! : string;
+  content! : string;
+
   constructor(public oidcSecurityService: OidcSecurityService) {}
 
   ngOnInit() {
 
   }
 
+  uploadImage($event: Event) {
+   const target = $event.target as HTMLInputElement;
+   const file: File = (target.files as FileList)[0];
+   console.log(file);
+  }
 }
