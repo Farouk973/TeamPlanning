e2dad73fae96960fa094b643d0c3c2917948306f
Raedjd <raedjaidi.work@gmail.com>
2023-03-06 21:31:33 +0100

    service change picture done

--- src/app/app-entry-point/dashboard/profile/profile.component.ts
+++ src/app/app-entry-point/dashboard/profile/profile.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
+import {User} from "../../../../shared/models/user.model";
+import {UserService} from "../../../../shared/services/user.service";
+import {Observable, Subscriber} from "rxjs";
 
 
 @Component({
@@ -10,10 +13,60 @@ import { OidcSecurityService } from 'angular-auth-oidc-client';
 export class ProfileComponent implements OnInit {
   profileJson: string | null = null;
 
-  constructor(public oidcSecurityService: OidcSecurityService) {}
+  currentUser!: User;
+  fileName!: string;
+  contentType!: string;
+  content!: string;
 
-  ngOnInit() {
-    
+  constructor(public oidcSecurityService: OidcSecurityService, public userService: UserService) {
   }
 
+  ngOnInit() {
+    // const userId = this.route.snapshot.paramMap.get('userId');
+    const userId = "6404c6f3f03762e5b9456b92"
+    this.userService.getUser(userId).subscribe(user => {
+      this.currentUser = user;
+    });
+  }
+
+  onFileSelected($event: any) {
+    const target = $event.target;
+    const file: File = target.files[0];
+    this.convertToBase64(file)
+    console.log(file)
+    this.fileName = file.name;
+    this.contentType = file.type;
+  }
+
+  private convertToBase64(file: File) {
+    const observable = new Observable((subscriber: Subscriber<any>) =>
+      this.readFile(file, subscriber));
+    observable.subscribe((d) => {
+      this.content = d;
+    })
+  }
+
+  private readFile(file: File, subscriber: Subscriber<any>) {
+    const filereader = new FileReader();
+    filereader.readAsDataURL(file);
+    filereader.onload = () => {
+      subscriber.next(filereader.result);
+      subscriber.complete();
+    }
+    filereader.onerror = () => {
+      subscriber.error();
+      subscriber.complete();
+    }
+  }
+
+  uploadFile() {
+    const data = {fileName: this.fileName, contentType: this.contentType, content: this.content}
+    console.log(data)
+    this.userService.updateImage(this.currentUser.id, data).subscribe(updatedImage => {
+      console.log('Image updated:', updatedImage);
+    });
+    this.userService.getUser(this.currentUser.id).subscribe(user => {
+      this.currentUser = user;
+    });
+  }
 }
