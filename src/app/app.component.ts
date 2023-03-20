80cd27f0a1f1f209bb30c355b2463ee79573a4e8
Hamza Bouhali <hbouhali@naxxum.fr>
2023-03-20 16:31:08 +0100

    fix : remove auth0 to test in kuber

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,31 +5,32 @@ import { OidcSecurityService } from 'angular-auth-oidc-client';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'team-planning';
   isAuthenticated = false;
-  constructor(
-    public oidcSecurityService: OidcSecurityService) {
-      this.oidcSecurityService.checkAuth()
-      .subscribe(({ isAuthenticated, userData, accessToken, idToken }) => {
-        if (!isAuthenticated) {
-          this.login();
-        } else {
-          console.log('current user:', userData);
-          this.isAuthenticated = true;
-        }
-      });
+  constructor(public oidcSecurityService: OidcSecurityService) {
+    // this.oidcSecurityService.checkAuth()
+    // .subscribe(({ isAuthenticated, userData, accessToken, idToken }) => {
+    //   if (!isAuthenticated) {
+    //     this.login();
+    //   } else {
+    //     console.log('current user:', userData);
+    //     this.isAuthenticated = true;
+    //   }
+    // });
+    this.isAuthenticated = true;
   }
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   login() {
     this.oidcSecurityService.authorize();
   }
 
   logout() {
-    this.oidcSecurityService.logoff().subscribe((result) => this.isAuthenticated = false);
+    this.oidcSecurityService
+      .logoff()
+      .subscribe((result) => (this.isAuthenticated = false));
   }
 }
