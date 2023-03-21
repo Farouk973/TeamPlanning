92c011550adbbf3af13c7459ca184390ed3d49f1
Mohamed Arab <marab@naxxum.fr>
2023-03-21 15:42:51 +0100

    generic button

--- src/app/app-entry-point/catalogue-services/catalogue-services/catalogue-services.component.ts
+++ src/app/app-entry-point/catalogue-services/catalogue-services/catalogue-services.component.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs/internal/Observable';
 import { HttpRepositoryService } from 'src/core/httpRepository.service';
 import { CardData } from 'src/shared/generic/generic_card/Models/cardModel';
 import {cards} from './cards'
-import { mockCardData } from './mock-data/data';
+import { categoryData, mockCardData } from './mock-data/data';
 
 @Component({
   selector: 'app-catalogue-services',
@@ -18,11 +18,12 @@ searchResults: any;
 search:string;
 private readonly apiUrl = 'api/Service';
 
-  cardDataList: CardData[];
-  cardDataMock: CardData=mockCardData;
+cardDataList: CardData[];
+cardDataMock: CardData=mockCardData;
+categories: CardData[] = categoryData;
  
   
-;
+
 handleSearchEvent($event: any) {
 throw new Error('Method not implemented.');
 }
@@ -43,13 +44,17 @@ throw new Error('Method not implemented.');
     return this.httpRepository.get<CardData[]>(this.apiUrl, params);
   }
   retrieveData(): void {
-    this.getData().subscribe((data: CardData[]) => {
-      this.cardDataList = data.map((cardData: CardData) => {
-        return { ...cardData, ...this.cardDataMock };
-      });
-      
+    this.getData().subscribe((cardData: CardData[]) => {
+      this.cardDataList = this.mapCardDataList(cardData);
+      console.log(this.cardDataList);
     });
   }
+  private mapCardDataList(cardData: CardData[]): CardData[] {
+  return cardData.map((data: CardData) => ({
+    ...data,
+    ...this.cardDataMock,
+  }));
+}
   }
   
 
--- src/app/app-entry-point/catalogue-services/catalogue-services/mock-data/data.ts
+++ src/app/app-entry-point/catalogue-services/catalogue-services/mock-data/data.ts
@@ -14,3 +14,46 @@ export const mockCardData: CardData={
     showIconAction: false,
   
   };
+  export const CommonCssBehaviours={
+    primaryColor: '#00b2d91a',
+    labelColor: '#00B2D9',
+
+  }
+  export const categoryData: CardData[]=[{
+   ...CommonCssBehaviours,
+   primaryLabel: 'Sport'
+    
+  
+  },
+  {
+    ...CommonCssBehaviours,
+    primaryLabel: 'Travel'
+     
+   
+   },
+   {
+    ...CommonCssBehaviours,
+    primaryLabel: 'Health'
+     
+   
+   },
+   {
+    ...CommonCssBehaviours,
+    primaryLabel: 'Love'
+     
+   
+   },
+   {
+    ...CommonCssBehaviours,
+    primaryLabel: 'Fashion'
+     
+   
+   },
+   {
+    ...CommonCssBehaviours,
+    primaryLabel: 'Technology'
+     
+   
+   }
+  
+  ];
