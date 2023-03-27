4327df3a37eb836df589ca90ca1967c07ca3a290
Zineddine Kermouz <zkermouz@naxxum.fr>
2023-03-27 13:03:27 +0000

    Updated environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,14 +2,14 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://localhost:44312',
+  baseUrl: 'http://20.74.17.113',
   authConfig: {
     domain: 'dev-jt2h2d8bnouk3jlk.us.auth0.com',
     authority: 'https://dev-jt2h2d8bnouk3jlk.us.auth0.com',
     clientId: '2NIVgvIcJ55BeqgjPsv4TDOXeIDVrTLF',
     scope: 'openid profile offline_access',
     audience: 'https://dev-jt2h2d8bnouk3jlk.us.auth0.com/api/v2/',
-    secureRoutes: ['https://localhost:44312/api/']
+    secureRoutes: ['http://20.74.17.113/api/']
   }
 };
 
