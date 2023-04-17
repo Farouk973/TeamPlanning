a76c2600fb38cec73c522d39384e6c08dcda3312
sabri krima <Skrima@naxxum.fr>
2023-04-17 05:20:49 +0100

    fix(teamPlanningFront) : fix conflits

--- src/shared/generic/form/form/form.component.ts
+++ src/shared/generic/form/form/form.component.ts
@@ -1,4 +1,4 @@
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import {
   Component,
   ElementRef,
@@ -8,11 +8,12 @@ import {
   Output,
   ViewChild,
 } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {debounceTime, tap, switchMap, finalize, distinctUntilChanged, filter} from 'rxjs/operators';
-import {Form} from '../../models/Form.model';
-import {SharedServices} from '../../SharedServices.service';
-import {ColumnMetadata} from '../form.service';
+import {  FormControl, FormGroup, Validators } from '@angular/forms';
+import { debounceTime, tap, switchMap, finalize, distinctUntilChanged, filter } from 'rxjs/operators';
+import { Form } from '../../models/Form.model';
+import { SharedServices } from '../../SharedServices.service';
+import { ColumnMetadata } from '../form.service';
+
 
 
 @Component({
@@ -21,12 +22,11 @@ import {ColumnMetadata} from '../form.service';
   styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
-  constructor(private formService: SharedServices, private http: HttpClient) {
-  }
+  constructor(private formService: SharedServices,private http: HttpClient) {}
 
   form = new FormGroup({});
   @Input() formData: Form;
-  @Output() myEvent = new EventEmitter<any>(true);
+  @Output() myEvent = new EventEmitter<any>();
   @ViewChild('buttonToSubmit') buttonToSubmit: ElementRef;
 
   @Input() set submitType(type: string) {
@@ -39,77 +39,75 @@ export class FormComponent implements OnInit {
   filteredData: any;
   isLoading = false;
   errorMsg!: string;
-  inputName: string = "";
+  inputName : string = "";
   minLengthTerm = 3;
   selectedMovieData: any = "";
   // formGroup = new FormGroup({});
-  x: any[]
   public isAllSelected: boolean;
 
   valueControl = new FormControl();
-
-  public removeOption(optionToRemove: any): void {
-    this.formData.Object['permissions'].setValue(this.formData.Object['permissions'].value.filter(option => optionToRemove.id !== option.id));
-  }
 
   ngOnInit(): void {
 
     this.formService.getMetadata(this.formData.metaData).subscribe((data) => {
       this.fields = data;
       this.fields.forEach((element) => {
-
+   
         if (element.type === 'array') {
-          this.ReferenceExistances(element.name);
+               this.ReferenceExistances(element.name);
+        }
+        if (element.type === null && element.format === null && element.description !== null &&!element.references[0]) {
+          console.log(element.name)
+          this.inputName=element.name
+          this.searchDataCtrl.setValue(this.formData.Object[this.inputName].title)    
+
         }
         this.createForm();
       });
     });
-
+   
     this.searchDataCtrl.valueChanges
-      .pipe(
-        filter(res => {
-          return res !== null && res.length >= this.minLengthTerm
-        }),
-        distinctUntilChanged(),
-        debounceTime(1000),
-        tap(() => {
-          this.errorMsg = "";
-          this.filteredData = [];
-          this.isLoading = true;
-        }),
-        switchMap(value => this.http.get('https://localhost:44312/api/' + this.inputName + '/' + value)
-          .pipe(
-            finalize(() => {
-              this.isLoading = false
-            }),
-          )
+    .pipe(
+      filter(res => {
+        return res !== null && res.length >= this.minLengthTerm
+      }),
+      distinctUntilChanged(),
+      debounceTime(1000),
+      tap(() => {
+        this.errorMsg = "";
+        this.filteredData = [];
+        this.isLoading = true;
+      }),
+      switchMap(value => this.http.get('https://localhost:44312/api/'+this.inputName+'/' + value)
+        .pipe(
+          finalize(() => {
+            this.isLoading = false
+          }),
         )
       )
-      .subscribe((data: any) => {
-        if (data == null) {
-          this.errorMsg = data['Error'];
-          this.filteredData = [];
-        } else {
-          this.errorMsg = "";
-          this.filteredData = data;
-        }
-      });
+    )
+    .subscribe((data: any) => {
+      if (data == null) {
+        this.errorMsg = data['Error'];
+        this.filteredData = [];
+      } else {
+        this.errorMsg = "";
+        this.filteredData = data;
+      }
+    });
 
   }
 
-  onSelected() {
-    this.selectedMovieData = this.selectedMovieData;
-  }
 
-  onselectData(value: string) {
+  onselectData(value : string){
     this.inputName = value;
+
   }
 
-  clearSelection() {
-    this.selectedMovieData = "";
-    this.filteredData = [];
-  }
 
+  displayDataTitle(data: any): string {
+    return data.title;
+  }
   createForm() {
 
     for (const field of this.fields) {
@@ -138,34 +136,30 @@ export class FormComponent implements OnInit {
         control.setValidators(Validators.compose(validators));
       }
       if (
-        this.formData.Object &&
+        this.formData.Object && 
         this.formData.Object.hasOwnProperty(field.name)
       ) {
         control.setValue(this.formData.Object[field.name]); // set the initial value from the data object
-
       }
-      if (field.type === 'array') {
-
+      if(field.type === 'array'){
+             
         const ids: string[] = this.formData.Object[field.name].map((object) => object.id);
-        control.setValue(ids); // set the initial value from the data object
-
-
+       control.setValue(ids); // set the initial value from the data object
       }
 
       this.form.addControl(field.name, control);
-      console.log(this.form.controls['role'])
     }
   }
 
   async submitAsync() {
-    this.buttonToSubmit?.nativeElement.click();
+    this.buttonToSubmit.nativeElement.click();
 
     return this.form.valid;
   }
 
   submitByType(type: string) {
     this.submitAsync().then((isValid) => {
-      console.log(isValid);
+      console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaa");
       console.log(this.form.value);
       console.log(type);
       if (isValid) {
@@ -187,17 +181,7 @@ export class FormComponent implements OnInit {
             );
         }
         if (!this.formData.Object && type == "CREATE") {
-          this.formService.addRow(this.formData.endpoint, this.form.value).subscribe({
-            next: (response) => {
-              console.log('response',response)
-              this.myEvent.emit({formValue: this.form.value, response: response,});
-            }, error: (error) => {
-              this.myEvent.emit({error: error})
-            }, complete: () => {
-              console.log('Row added successfully.');
-            },
-          });
-          /*this.formService
+          this.formService
             .addRow(this.formData.endpoint, this.form.value)
             .subscribe(
               (response) => {
@@ -208,7 +192,7 @@ export class FormComponent implements OnInit {
               },
               (error) => {
               }
-            );*/
+            );
         }
       }
     });
@@ -251,16 +235,17 @@ export class FormComponent implements OnInit {
       }
     });
   }
+  ReferenceExistances(desc :string) :any
+  {
+  this.formService.getData("https://localhost:44312/api/"+desc)
+  .subscribe({
+    next: (data) => {
+      this.map.set(desc,data);
 
-  ReferenceExistances(desc: string): any {
-    this.formService.getData("https://localhost:44312/api/" + desc)
-      .subscribe({
-        next: (data) => {
-          this.map.set(desc, data);
-
-        },
-      })
+    },
+  })
   }
 
 
+  
 }
