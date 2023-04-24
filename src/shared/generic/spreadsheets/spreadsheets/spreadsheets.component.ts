fb847aaac7eb5494a767994544ca9b1cafd25cc7
Raedjd <raedjaidi.work@gmail.com>
2023-04-24 17:29:11 +0100

    feat(costing progress in progress)

--- src/shared/generic/spreadsheets/spreadsheets/spreadsheets.component.ts
+++ src/shared/generic/spreadsheets/spreadsheets/spreadsheets.component.ts
@@ -6,6 +6,7 @@ import {Location} from "@angular/common";
 import {Observable} from "rxjs";
 import {Spreadsheets} from "../../models/Spreadsheets.model";
 import {Router} from "@angular/router";
+import {logExperimentalWarnings} from "@angular-devkit/build-angular/src/builders/browser-esbuild/experimental-warnings";
 
 
 @Component({
@@ -55,6 +56,8 @@ export class SpreadsheetsComponent implements  OnChanges {
       })
 
 
+
+
     })
 
 
@@ -76,8 +79,11 @@ export class SpreadsheetsComponent implements  OnChanges {
         this.editMode[rowIndex].fill(!this.editMode[rowIndex][0]);
         console.log("data",this.data.reportData)
       }
+
     }
     if(!this.editModeStyle){
+      const url = this.location.path();
+      let id = url.substring(url.lastIndexOf('/') + 1);
       this.chiffrage = []
       for (let row of this.data.reportData) {
         const obj = {};
@@ -86,8 +92,27 @@ export class SpreadsheetsComponent implements  OnChanges {
         }
         this.chiffrage.push(obj);
       }
-      console.log("chiffrage",this.chiffrage)
-    }
+      console.log(this.chiffrage)
+
+      this.spreadsheetsService.addCosting('/api/Project/addCostingToProject' , id , this.chiffrage ).subscribe( (response)=>
+        console.log(response))
+
+      this.spreadsheetsService.getItem(this.spreadsheets.rowHeaderEndpoint,id).subscribe((data)=>{
+        const numCols = Object.keys(data.costing[0]).length;
+        for (let i = 0; i < data.costing.length; i++) {
+          const row = [];
+          for (let j = 0; j < numCols; j++) {
+            const colName = 'role' + j;
+            row.push(data.costing[i][colName]);
+          }
+          this.data.reportData.push(row);
+        }
+      })
+
+
+      }
+
+
 
     }
 
