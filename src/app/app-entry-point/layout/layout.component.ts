cabd8624cabb63a4d61b20ebae6be761aaf18d60
Raedjd <raedjaidi.work@gmail.com>
2023-05-19 12:13:27 +0100

    feat( teamPlanning front : dynamic notification done)

--- src/app/app-entry-point/layout/layout.component.ts
+++ src/app/app-entry-point/layout/layout.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MediaObserver } from '@angular/flex-layout';
 import { Subscription } from 'rxjs';
+import {OidcSecurityService} from "angular-auth-oidc-client";
+import {NotificationService} from "../../../shared/services/notification.service";
+import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from "@angular/material/snack-bar";
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
@@ -10,8 +13,14 @@ export class LayoutComponent implements OnInit, OnDestroy {
   sideNavOpened = true;
   sideNavMode: 'side' | 'over' = 'side';
   toolBarHeight = 64;
+  notification : string;
+  connectedUserId : string;
+  userId: string;
   private readonly mediaWatcher: Subscription;
-  constructor(media: MediaObserver) {
+  horizontalPosition: MatSnackBarHorizontalPosition = 'start';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+  constructor(private _snackBar: MatSnackBar ,media: MediaObserver ,   public oidcSecurityService: OidcSecurityService ,private notificationService : NotificationService
+  ) {
     this.mediaWatcher = media.asObservable().subscribe((changes) => {
       changes.forEach(change => {
         if (change.mqAlias === 'sm' || change.mqAlias === 'xs') {
@@ -31,8 +40,30 @@ export class LayoutComponent implements OnInit, OnDestroy {
       });
     });
   }
-  ngOnInit() { }
+  ngOnInit() {
+    this.notificationService.getNewNotification().subscribe((notification : string)=>{
+      // this.notifications.push(notification)
+      const indice =notification.indexOf("auth0|");
+      this.notification = notification.substring(0, indice);
+      this.connectedUserId = notification.substring(indice);
+      console.log('connected',this.connectedUserId)
+      this.openSnackBar(this.notification , this.connectedUserId);
+    })
+    this.oidcSecurityService.checkAuth()
+      .subscribe(({userData,  }) => {
+        this.userId = userData.sub;
+        console.log(this.userId)
 
+      });
+
+  }
+  openSnackBar(notif : any , userId : any) {
+    if(this.userId == userId){
+    this._snackBar.open(notif, 'close', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });}
+  }
   ngOnDestroy(): void {
     this.mediaWatcher.unsubscribe();
   }
