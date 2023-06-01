6a293d125e5b6b9bcc0480aa2197dac8a4f27d5f
Raedjd <raedjaidi.work@gmail.com>
2023-06-01 14:25:54 +0100

    fix (teamPlanning front : fix stepper for add request)

--- src/app/app-entry-point/request-managment/add-request/add-request.component.ts
+++ src/app/app-entry-point/request-managment/add-request/add-request.component.ts
@@ -2,6 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Stepper } from 'src/shared/generic/models/stepper.model';
+import {FormComponent} from "../../../../shared/generic/form/form/form.component";
+import {FeatureComponent} from "../../project-managment/feature-managment/feature/feature.component";
+import {RolesComponent} from "../../project-managment/roles-management/roles/roles.component";
+import {SkillStepperComponent} from "../../skill-stepper-management/skill-stepper/skill-stepper.component";
+import {RecopProjectComponent} from "../../project-managment/recop-project/recop-project.component";
 
 @Component({
   selector: 'app-add-request',
@@ -18,5 +23,15 @@ export class AddRequestComponent implements OnInit {
   ngOnInit(): void {
     this.stepper$=this.http.get<Stepper>('/assets/requestStepper.json');
   }
+  getComponent(contentType: string): any {
+    switch (contentType) {
+      case 'Form':
+        return FormComponent;
+      case 'Features':
+        return FeatureComponent;
 
+      default:
+        throw new Error(`Invalid component name: ${contentType}`);
+    }
+  }
 }
